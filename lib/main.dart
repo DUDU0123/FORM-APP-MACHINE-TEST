550886e260eb2a86b8ead30e6882962a64a1540d
DUDU0123 <sdu200115@gmail.com>
2024-10-29 22:06:41 +0530

    Some minor changes.

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:form_app_machine_task/root_widget_page.dart';
 
-void main(){
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(ProviderScope(child: const RootWidgetPage()));
+  runApp(
+    const ProviderScope(
+      child: RootWidgetPage(),
+    ),
+  );
 }
-
